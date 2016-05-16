b157e14760cc5b7a4be0498f230a976ae2f27f00
Fred <fred.chou.nd@gmail.com>
2016-05-16 16:05:41 +0800

    use f0.5 as score

--- learn.py
+++ learn.py
@@ -1,6 +1,15 @@
 
 import pandas as pd
 
+def performance_metric(y_true, y_pred):
+	from sklearn.metrics import precision_score, recall_score
+	w=0.5
+	w = w**2
+	p = precision_score(y_true, y_pred, average='micro') 
+	r = recall_score(y_true, y_pred, average='micro') 
+	f = (1+w)*p*r/(w*p+r)
+	return f
+
 def main():
 	
 	fname = 'data_train.csv'
@@ -18,6 +27,30 @@ def main():
 	print "Total number of data points: {}".format(len(data))
 	print "Number of data that should trigger buy: {}".format(data[data.should_buy==1].shape[0]) 
 	print "Number of data that should not trigger buy: {}".format(data[data.should_buy==0].shape[0])
+
+
+	# Data is unbalanced
+	# how to do CV
+
+
+	print data.head(5)
+
+	# drop volume, as more than half are missing
+	print data['vol_0'].describe()
+	data = data.drop(['vol_0', 'vol_1', 'vol_2', 'vol_3'], axis=1)
+
+	# drop date
+	data = data.drop(['date'], axis=1)
+
+	# drop reward
+	data = data.drop(['reward'], axis=1)
+
+	# drop adj_close
+	data = data.drop(['adj_close_0', 'adj_close_1', 'adj_close_2', 'adj_close_3'], axis=1)
+
+
+
+	print data.head(5)
 
 	print 'Done!'
 
